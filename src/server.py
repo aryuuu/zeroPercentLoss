caf2d1504221f96d796314f8ac3911ea5432fc64
Fajar <13517149@std.stei.itb.ac.id>
2019-09-24 06:17:34 +0700

    server can handle multiple sender

--- src/server.py
+++ src/server.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 import socket
 from packethandler import *
+import struct
+ 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(('', 2000))
+# wrong: mreq = struct.pack("sl", socket.inet_aton("224.51.105.104"), socket.INADDR_ANY)
+mreq = struct.pack("=4sl", socket.inet_aton("224.51.105.104"), socket.INADDR_ANY)
 
-#test push
-#create socket object
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-#set host and port
-host = input("host >> ")
-port = int(input("port >> "))
-
-
-#bind to the port
-server_socket.bind((host,port))
 print("server is ready")
 
 #queue up to 5 requests
